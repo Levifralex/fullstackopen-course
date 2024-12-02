91d2740aa0370b92dd39ad8ce8cc69413fb648ba
Leonardo Villegas <leon1010@hotmail.com>
2024-12-02 15:06:17 -0500

    new (part4): blog project config & db connection

---
+++ part4/blogs/models/blog.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  likes: {
+    type: Number,
+    required: true
+  },
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', noteSchema)
