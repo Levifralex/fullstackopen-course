f850461f194c90d0669030ae6f2b2e8bc8ceb52c
Leonardo Villegas <leon1010@hotmail.com>
2024-12-02 16:47:08 -0500

    new (part4): add controller to project and refactoring

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -2,34 +2,6 @@ const app = require('./app') // The Express app
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const middleware = require('./utils/middleware')
-
-const Blog = require('./models/blog')
-
-app.use(middleware.requestLogger)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
