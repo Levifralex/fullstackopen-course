40b92bf5adf595b62fce722fbdf53ed538753c0a
Leonardo Villegas <leon1010@hotmail.com>
2024-12-02 15:33:03 -0500

    new (part4): add middleware to project

--- part4/blogs/app.js
+++ part4/blogs/app.js
@@ -2,7 +2,6 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
 const logger = require('./utils/logger')
 const mongoose = require("mongoose");
 
--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -2,7 +2,11 @@ const app = require('./app') // The Express app
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
+const middleware = require('./utils/middleware')
+
 const Blog = require('./models/blog')
+
+app.use(middleware.requestLogger)
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -21,6 +25,10 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
+
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
