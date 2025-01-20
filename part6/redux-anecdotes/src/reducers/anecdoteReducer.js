a305c48e3006448c3a1be4284e44fae922c061e8
Leonardo Villegas <leon1010@hotmail.com>
2025-01-20 09:27:04 -0500

    new (part6): exercise 6.11

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,4 @@
-/* import { createSlice, current } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const anecdotesAtStart = [
   "If it hurts, do it more often",
@@ -26,102 +26,29 @@ const anecdoteSlice = createSlice({
   initialState,
   reducers: {
     createAnecdote(state, action) {
-      const anecdote = action.payload;
+      const content = action.payload;
       state.push({
-        content: anecdote,
+        content,
         id: getId(),
         votes: 0,
       });
     },
     voteAnecdote(state, action) {
-      const id = action.payload;
+      const id = action.payload
+      const anecdoteToChange = state.find(n => n.id === id)
+      const changedAnecdote = { 
+        ...anecdoteToChange, 
+        votes: anecdoteToChange.votes + 1
+      }
       
-      console.log('id :>> ', id);
-      console.log("state => ", current(state))
+      const anecdotes = state.map(element =>
+        element.id !== id ? element : changedAnecdote 
+      )
 
-      const anecdoteToVote = state.find((n) => n.id === id);
-      console.log('anecdoteToVote :>> ', anecdoteToVote);
-      
-      //increase by 1 value of array element by id
-      const changedAnecdote = {
-        ...anecdoteToVote,
-        votes: ++anecdoteToVote.votes,
-      };
-
-      //return modified anecdote array order by votes
-      return state.map((anecdote) =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      );
+      return [...anecdotes].sort((a, b) => b.votes - a.votes);
     }
   }
 })
 
-export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
-export default anecdoteSlice.reducer */
-
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "NEW_ANECDOTE":
-      return [...state, action.payload];
-    case "VOTE": {
-      //search anecdote by id
-      const id = action.payload.id;
-      const anecdoteToVote = state.find((n) => n.id === id);
-
-      //increase by 1 value of array element by id
-      const changedAnecdote = {
-        ...anecdoteToVote,
-        votes: ++anecdoteToVote.votes,
-      };
-
-      //return modified anecdote array
-      return state.map((anecdote) =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      ).sort((a, b) => b.votes - a.votes);
-    }
-    default:
-      return state;
-  }
-};
-
-//action creators
-export const voteAnecdote = (id) => {
-  return {
-    type: "VOTE",
-    payload: { id },
-  };
-};
-
-export const createAnecdote = (anecdote) => {
-  return {
-    type: "NEW_ANECDOTE",
-    payload: {
-      content: anecdote,
-      id: getId(),
-      votes: 0,
-    },
-  };
-};
-
-export default reducer;
+export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,30 +1,10 @@
-/* const filterReducer = (state = "", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-//action creators
-export const filterChange = (filter) => {
-  return {
-    type: "SET_FILTER",
-    payload: filter,
-  };
-};
-
-export default filterReducer; */
-
-import { createSlice, current } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const filterSlice = createSlice({
   name: "filter",
   initialState: '',
   reducers: {
     filterChange(state, action) {
-      console.log(current(state))
       return action.payload;
     },
   },
